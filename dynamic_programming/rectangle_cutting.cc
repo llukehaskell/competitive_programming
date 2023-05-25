7653a1a5575b9add76390c63848339a5c6f35415
llukehaskell <lukehaskell@gmail.com>
2023-05-25 16:22:54 -0400

    rectangle cut kinda working

--- dynamic_programming/rectangle_cutting.cc
+++ dynamic_programming/rectangle_cutting.cc
@@ -2,19 +2,45 @@
 #include <iostream>
 #include <vector>
 
+const int INF = 32000;
+
 int main() {
     int a, b, counter = 0;
     std::cin >> a >> b;
 
-    while (a != b) {
-        if (a > b) {
-            a -= b;
-        } else {
-            b -= a;
-        }
-        counter++;
+    std::vector<std::vector<int>> min_cut(a + 1, std::vector<int>(b + 1, INF));
+
+    // marking off the diagonal since we know squares don't need to be looked at any longer (cut count of 0)
+    for (int i = 0; i <= std::min(a, b); ++i) {
+        min_cut[i][i] = 0;
     }
-    std::cout << counter;
+
+    // for every spot in the matrix
+    for (int i = 1; i <= a; ++i) {
+        for (int j = 1; j <= b; ++j) {
+            
+            // for every spot find the minimum previous solution
+            for (int k = 1; k < a; ++k) {
+                min_cut[i][j] = std::min(
+                    min_cut[i][j],
+                    min_cut[k][j] + min_cut[i - k][j] + 1);
+            }
+            for (int k = 1; k < b; ++k) {
+                min_cut[i][j] = std::min(
+                    min_cut[i][j],
+                    min_cut[i][k] + min_cut[i][j - k] + 1);
+            }
+        }
+    }
+
+    // for (int i = 0; i <= a; ++i){
+    //     for (int j = 0; j <= b; ++j){
+    //         std::cout << min_cut[i][j] << " ";
+    //     }
+    //     std::cout << '\n';
+    // }
+
+    std::cout << min_cut[a][b];
 }
 
 
