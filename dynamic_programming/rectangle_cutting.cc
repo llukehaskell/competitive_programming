0c8aa044a18daea2aacefc92b08046a6a2d88946
llukehaskell <lukehaskell@gmail.com>
2023-05-25 16:52:42 -0400

    working

--- dynamic_programming/rectangle_cutting.cc
+++ dynamic_programming/rectangle_cutting.cc
@@ -14,18 +14,17 @@ int main() {
     for (int i = 0; i <= std::min(a, b); ++i) {
         min_cut[i][i] = 0;
     }
-
     // for every spot in the matrix
     for (int i = 1; i <= a; ++i) {
         for (int j = 1; j <= b; ++j) {
             
             // for every spot find the minimum previous solution
-            for (int k = 1; k < a; ++k) {
+            for (int k = 1; k < i; ++k) {
                 min_cut[i][j] = std::min(
                     min_cut[i][j],
                     min_cut[k][j] + min_cut[i - k][j] + 1);
             }
-            for (int k = 1; k < b; ++k) {
+            for (int k = 1; k < j; ++k) {
                 min_cut[i][j] = std::min(
                     min_cut[i][j],
                     min_cut[i][k] + min_cut[i][j - k] + 1);
@@ -33,16 +32,10 @@ int main() {
         }
     }
 
-    // for (int i = 0; i <= a; ++i){
-    //     for (int j = 0; j <= b; ++j){
-    //         std::cout << min_cut[i][j] << " ";
-    //     }
-    //     std::cout << '\n';
-    // }
-
     std::cout << min_cut[a][b];
 }
 
+// credit to ARS LONGA VITA BREVIS on yt for help
 
 /*
 g++ -o rectangle_cutting.out rectangle_cutting.cc && ./rectangle_cutting.out
