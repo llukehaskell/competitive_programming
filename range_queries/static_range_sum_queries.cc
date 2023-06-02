400b19305b8db916f6d92cb0cd6cd23f5d896902
llukehaskell <lukehaskell@gmail.com>
2023-06-02 14:38:19 -0400

    accepted

--- range_queries/static_range_sum_queries.cc
+++ range_queries/static_range_sum_queries.cc
@@ -9,24 +9,18 @@ int main() {
     // setup
     int n, q, a, b = 0;
     cin >> n >> q;
-    ll arr[n];
-    ll res[q];
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
+    ll arr[n + 1];
 
     // main
+    arr[0] = 0; // fill first slot of array
+    for (int i = 1; i <= n; ++i) {
+        cin >> arr[i];
+        arr[i] += arr[i - 1]; // add previous entry to current to create an increasing list of numbers
+    }
     for (int i = 0; i < q; i++) {
         cin >> a >> b;
-        res[i] = 0;
-        for (int j = (a - 1); j <= (b - 1); ++j) {
-            res[i] += arr[j];
-        }
-    }
-
-    // print
-    for (int i : res) {
-        cout << i << "\n";
+        // print (must be like this and not an array of answers bc of big number overflow stuff)
+        cout << (arr[b] - arr[a - 1]) << "\n"; // super clever and smart math
     }
 }
 
