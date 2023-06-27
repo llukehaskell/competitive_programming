b901128cb6781826f11fb929ff180b2a81118a6f
llukehaskell <lukehaskell@gmail.com>
2023-06-27 11:44:34 -0400

    unfinished

---
+++ range_queries/range_updates_and_sums.cc
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+    int n, q = 0; // array size, # of queries
+    ll x = 0; // updated value
+    cin >> n >> q;
+    ll arr[n * 2]; // segment tree setup (index 0 never used)
+
+    
+}
+
+/*
+g++ -o range_updates_and_sums.out range_updates_and_sums.cc && ./range_updates_and_sums.out
+*/
